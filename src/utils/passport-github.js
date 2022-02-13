429b4a66a9e8994a76b544605d18b15f99cbef5e
lassev05 <vestergaardlasse2@gmail.com>
2022-02-13 18:55:03 +0100

    update passport-github.js

--- src/utils/passport-github.js
+++ src/utils/passport-github.js
@@ -8,7 +8,7 @@ passport.use(
         {
             clientID: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            callbackURL: "http://localhost:3000/auth/github/callback",
+            callbackURL: "/auth/github/callback",
         },
         async function (accessToken, refreshToken, profile, done) {
             const newUser = {
