7aca803a93d07e3e708dea88cb57ec2ee0c5c2b0
lassev05 <vestergaardlasse2@gmail.com>
2022-02-20 16:58:18 +0100

    make it better

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -45,6 +45,11 @@ const UserSchema = new mongoose.Schema({
         type: Array,
     },
 
+    banned: {
+        type: Boolean,
+        default: false,
+    },
+
     joinedAt: {
         type: String,
         required: true,
